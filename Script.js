80895a5ad57fe79a2c268fed3c95e445e36c4244
Raashad2004 <mohammedraashad10@gmail.com>
2025-03-22 16:11:59 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.btn').forEach(button => {
-  button.addEventListener('click', () => {
-      alert('Thank you for your inquiry! We will get back to you shortly.');
-  });
-});
