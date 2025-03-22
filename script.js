a2089f3cb2c3681bdeaa7b83ff91aae9cb9d7fa8
Raashad2004 <mohammedraashad10@gmail.com>
2025-03-22 16:12:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+      alert('Thank you for your inquiry! We will get back to you shortly.');
+  });
+});
